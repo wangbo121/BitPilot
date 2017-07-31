34a64966c08f6f92f9fa2704bc28bc7d236e584e
wangbo121 <wangbo121@189.cn>
2017-07-31 13:26:43 +0800

    增加了global.h和defines.h两个空文件

---
+++ defines.h
@@ -0,0 +1,15 @@
+/*
+ * defines.h
+ *
+ *  Created on: 2017-7-31
+ *      Author: wangbo
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+
+
+
+
+#endif /* DEFINES_H_ */
---
+++ global.h
@@ -0,0 +1,15 @@
+/*
+ * global.h
+ *
+ *  Created on: 2017-7-31
+ *      Author: wangbo
+ */
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+
+
+
+
+#endif /* GLOBAL_H_ */
