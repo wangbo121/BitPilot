40734b0e2974d30e3cf89f137ba41c683a70f9e9
wangbo121 <wangbo121@189.cn>
2017-07-31 13:43:13 +0800

    增加了vector2.h和vector3.h编译通过，可以用这个来作为向量了

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 #include "pid.h"
+#include "vector2.h"
 
 
 
@@ -24,6 +25,17 @@ int main()
 	float result = pid.get_pid(2,1000,1);
 
 	cout<<"result="<<result<<endl;
+
+
+	Vector2i vector2_1(1,2);
+	Vector2i vector2_2(3,4);
+	Vector2i vector_result;
+
+	vector_result=vector2_1+vector2_2;
+
+	cout<<vector_result.x<<endl;
+	cout<<vector_result.y<<endl;
+
 
 
 	return 0;
