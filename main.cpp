18d2e25124d3e3465822fd7b116d392f90c43b40
wangbo121 <wangbo121@189.cn>
2017-07-31 13:49:43 +0800

    修改了vector3.h文件中构造函数赋值的错误，在main函数中加了vector3的验证

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 #include "pid.h"
 #include "vector2.h"
+#include "vector3.h"
 
 
 
@@ -20,21 +21,31 @@ int main()
 	cout<<"Welcome to BitPilot"<<endl;
 
 	BIT_PID pid;
-
 	pid.set_kP(2);
-	float result = pid.get_pid(2,1000,1);
-
-	cout<<"result="<<result<<endl;
+	float pid_result = pid.get_pid(2,1000,1);
+	cout<<"pid_result="<<pid_result<<endl;
 
 
 	Vector2i vector2_1(1,2);
 	Vector2i vector2_2(3,4);
-	Vector2i vector_result;
+	Vector2i vector2_result;
 
-	vector_result=vector2_1+vector2_2;
+	vector2_result=vector2_1+vector2_2;
 
-	cout<<vector_result.x<<endl;
-	cout<<vector_result.y<<endl;
+	cout<<"vector2_result.x="<<vector2_result.x<<endl;
+	cout<<"vector2_result.y="<<vector2_result.y<<endl;
+
+	Vector3i vector3_1(1,2,3);
+	Vector3i vector3_2(3,4,5);
+	Vector3i vector3_result;
+
+	vector3_result=vector3_1+vector3_2;
+
+	cout<<"vector3_result.x="<<vector3_result.x<<endl;
+	cout<<"vector3_result.y="<<vector3_result.y<<endl;
+	cout<<"vector3_result.z="<<vector3_result.z<<endl;
+
+
 
 
 
