618b84e6dcb9f52135da04acf1b45adaa0dc63a3
wangbo121 <wangbo121@189.cn>
2017-07-31 12:26:37 +0800

    添加了pid文件和修改了main.cpp，能够正常编译，没有问题

--- main.cpp
+++ main.cpp
@@ -5,6 +5,29 @@
  *      Author: wangbo
  */
 
+#include <iostream>
+
+using namespace std;
+
+#include "pid.h"
+
+
+
+int main()
+{
+
+	cout<<"Welcome to BitPilot"<<endl;
+
+	BIT_PID pid;
+
+	pid.set_kP(2);
+	float result = pid.get_pid(2,1000,1);
+
+	cout<<"result="<<result<<endl;
+
+
+	return 0;
+}
 
 
 
---
+++ pid.h
@@ -0,0 +1,55 @@
+/*
+ * pid.h
+ *
+ *  Created on: 2017-7-31
+ *      Author: wangbo
+ */
+
+#ifndef PID_H_
+#define PID_H_
+
+#include <inttypes.h>
+#include <math.h>		// for fabs()
+
+//实例化的对象只对应(管理)一个 PID 控制器
+class BIT_PID {
+public:
+	BIT_PID();
+
+	float 	get_pid(int32_t error, uint16_t dt_ms, float scaler = 1.0);
+
+	/// Reset the PID integrator
+	///
+	void	reset_I();
+
+	void	set_kP(const float v)		{ _kp = v; }
+	void	set_kI(const float v)		{ _ki = v; }
+	void	set_kD(const float v)		{ _kd = v; }
+	void	set_imax(const int16_t v)	{ _imax = v; }
+
+	float	get_kP()			{ return _kp; }
+	float	get_kI()			{ return _ki; }
+	float	get_kD()			{ return _kd; }
+	float	get_imax()			{ return _imax; }
+
+	float	get_integrator() const	{ return _integrator; }
+
+private:
+	float				_kp;
+	float				_ki;
+	float				_kd;
+	float				_imax;
+
+	float				_integrator;		///< integrator value
+	int32_t			_last_error;		///< last error for derivative
+	float				_last_derivative; 	///< last derivative for low-pass filter
+
+	/// Low pass filter cut frequency for derivative calculation.
+	///
+	/// 20 Hz becasue anything over that is probably noise, see
+	/// http://en.wikipedia.org/wiki/Low-pass_filter.
+	///
+	static const uint8_t _fCut = 20;
+};
+
+#endif /* PID_H_ */
