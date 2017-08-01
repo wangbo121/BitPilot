df3c35958468cd4759ee894a37c61b7def55097a
wangbo121 <wangbo121@189.cn>
2017-08-01 16:24:09 +0800

    建立了attitude control_stabilize两个空文件

---
+++ attitude.cpp
@@ -0,0 +1,10 @@
+/*
+ * attitude.cpp
+ *
+ *  Created on: 2017-8-1
+ *      Author: wangbo
+ */
+
+
+
+
---
+++ control_stabilize.cpp
@@ -0,0 +1,10 @@
+/*
+ * control_stabilize.cpp
+ *
+ *  Created on: 2017-8-1
+ *      Author: wangbo
+ */
+
+
+
+
